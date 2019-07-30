b14f6e86cb4f6630133437bd9a35b06d1f89ceac
Daniel Eze <dan4allu93@gmail.com>
2019-07-30 14:52:31 +0100

    Updates

---
+++ precache-manifest.02c02169a46c1d6413f3c6a748abb64f.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "75acd23d9f53b02191fcf626d37f8736",
+    "url": "/dribbble-redesign/index.html"
+  },
+  {
+    "revision": "ddc960da863eac9b899a",
+    "url": "/dribbble-redesign/static/css/main.7576f203.chunk.css"
+  },
+  {
+    "revision": "1cf1afcfe66bf6f5381e",
+    "url": "/dribbble-redesign/static/js/2.ef7a9a4f.chunk.js"
+  },
+  {
+    "revision": "ddc960da863eac9b899a",
+    "url": "/dribbble-redesign/static/js/main.c72d9d0f.chunk.js"
+  },
+  {
+    "revision": "230da3cebea07aff4d98",
+    "url": "/dribbble-redesign/static/js/runtime~main.aa42e57f.js"
+  },
+  {
+    "revision": "2fdd6c668f7bb12d00d35bfa5ef29b6d",
+    "url": "/dribbble-redesign/static/media/NHaasGroteskTXPro-55Rg.2fdd6c66.ttf"
+  },
+  {
+    "revision": "6131025d427429fde8ca76b8890016c2",
+    "url": "/dribbble-redesign/static/media/NHaasGroteskTXPro-75Bd.6131025d.ttf"
+  },
+  {
+    "revision": "c45121124093062b4b4e7adf6e1e64bb",
+    "url": "/dribbble-redesign/static/media/avatar.c4512112.jpg"
+  },
+  {
+    "revision": "651e75a64ce766458aeb6b58a0aa377e",
+    "url": "/dribbble-redesign/static/media/dribbble.651e75a6.svg"
+  },
+  {
+    "revision": "7fc8ff75d11708e40a5eef75e976456f",
+    "url": "/dribbble-redesign/static/media/invoice_maker_4x.7fc8ff75.jpg"
+  },
+  {
+    "revision": "23386ed5940d6665804594f4ba58a8f1",
+    "url": "/dribbble-redesign/static/media/pablo-basketball-game.23386ed5.png"
+  },
+  {
+    "revision": "41db05b4eb9c16dcf45e9ace86bb5606",
+    "url": "/dribbble-redesign/static/media/pablo-car-rental.41db05b4.png"
+  },
+  {
+    "revision": "024dfd29f8c88ceb321f67245a57a603",
+    "url": "/dribbble-redesign/static/media/pablo-cinema.024dfd29.png"
+  },
+  {
+    "revision": "49716ec477b24aa5a02de31720ce6729",
+    "url": "/dribbble-redesign/static/media/pablo-family.49716ec4.png"
+  },
+  {
+    "revision": "ccbe862107d7469909bf68e8ac0e389d",
+    "url": "/dribbble-redesign/static/media/pablo-healthy-life.ccbe8621.png"
+  },
+  {
+    "revision": "5bf0b66cae21ef7d148ed654685fd8dc",
+    "url": "/dribbble-redesign/static/media/pablo-unsubscribed.5bf0b66c.png"
+  },
+  {
+    "revision": "c0a8206e8a0dceae45aa40650fcb9019",
+    "url": "/dribbble-redesign/static/media/pablo-workshop.c0a8206e.png"
+  }
+]);
